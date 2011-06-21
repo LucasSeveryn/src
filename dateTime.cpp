c09fc5e924aa53dce4641d2283b13382915b8656
Lukasz Seweryn <moa.1991@gmail.com>
2011-06-21 21:31:46 +0100

    all commits

--- dateTime.cpp
+++ dateTime.cpp
@@ -8,20 +8,16 @@ using namespace std;
 
 DateTime::DateTime(string input){
 	string delims="/ :";
-//	int args[6];
-	int args[6]={20,12,1991,9,45,60};
-	string token;
+	int args[6];
 
-	/*
-	getline(input, token, ' ');
 	args[0]=atoi(strtok(input,delims));
 	for(int i=1;i<6;i++){
 		args[i]=atoi(strtok(NULL,delims));
-	}*/
+	}
 
 
 
-	this->setDateTime(args[0],args[1],args[2],args[3],args[4],args[5]);
+	//this->setDateTime(args[0],args[1],args[2],args[3],args[4],args[5]);
 }
 
 void DateTime::setDateTime(int d,int mon,int y, int h, int min, int s){
